f9240ff6d5bcf9854fb57818ce1fbc7de4e42553
Nitish232 <samanthakurthinitish@gmail.com>
2024-05-04 19:46:58 +0530

    m

---
+++ mysql.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+userid=$(id -u)
+timestap=(date +%F-%H-%M-%S)
+script_name=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$script_name-$timestap.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Please enter DB password : "
+read -s mysql_root_password
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2...$R  FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+
+}
+
+if [ $userid -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "You are super user."
+fi
+
+dnf install mysql-server -y &>>$logfile
+VALIDATE $? "Installing Mysql Server"
+
+systemctl enable mysqld &>>$logfile
+VALIDATE $? "Enabling Mysql Server"
+
+systemctl start mysqld &>>$logfile
+VALIDATE $? "Starting mysql Server"
+
+mysql -h 172.31.27.78 -uroot -p${mysql_root_password} -e 'show databases;' &>>$logfile
+if [ $? -ne 0 ]
+then 
+    mysql_root_password --set-root-pass ${mysql_root_password} &>>$logfile
+    VALIDATE $? "Mysql root password setup"
+else
+    echo -e "Mysql root Password is already setup...$Y SKIPPING $N"
+fi
